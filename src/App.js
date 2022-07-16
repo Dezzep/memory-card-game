aee9841cb289f0c037c179120ac75412718cd66e
Jacob Handelman <jacobhandelman@gmail.com>
2022-07-16 16:50:52 -0400

    the game starts by rendering 4 cards, once the score is at 4, it renders 8.. then it'll render 12, then 16.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ScoreTracker from './components/scoreTracker';
 import Deck from './components/all_cards';
 
@@ -10,11 +10,18 @@ const App = () => {
   const renderMoreCards = (currentScore) => {
     if (currentScore === cardsToRender && cardsToRender <= 16)
       setCardsToRender(cardsToRender + 4)
+    else if (currentScore < 4) {
+      setCardsToRender(4);
+    }  
   }
 
   const scoreReceivedFromDeck = (deckScore = 0) => {
     setScore(deckScore);
   }
+  useEffect(() => {
+    renderMoreCards(score)
+  })
+
 
   return (
     <div>
--- src/components/all_cards.js
+++ src/components/all_cards.js
@@ -1,40 +1,71 @@
 import Card from "./card";
-import { useState, } from "react";
+import { useEffect, useState, } from "react";
 
 const Deck = (props) => {
-
   const [selectedCards, setSelectedCards] = useState([]);
- 
+
   const receiveIdFromCard = (idOfCard) => {
     if (selectedCards.includes(idOfCard)){
       setSelectedCards([])
       props.sendScore(0);
-      //might need to set game over status too.
     }
     else{
     setSelectedCards([...selectedCards, idOfCard])
     props.sendScore(selectedCards.length + 1)
     }
   }
-  const arrayOfAllCards = [<Card key='1'  cardID='1'  sendIdToDeck={receiveIdFromCard} image={'Arryn.png'}/>,
-  <Card key='2'   cardID='2'  sendIdToDeck={receiveIdFromCard} image={'Baratheon.png'}/>,
-  <Card key='3'   cardID='3'  sendIdToDeck={receiveIdFromCard} image={'Brynden.png'}/>,
-  <Card key='4'   cardID='4'  sendIdToDeck={receiveIdFromCard} image={'Clegane.png'}/>,
-  <Card key='5'   cardID='5'  sendIdToDeck={receiveIdFromCard} image={'direwolf.png'}/>,
-  <Card key='6'   cardID='6'  sendIdToDeck={receiveIdFromCard} image={'Greyjoy.png'}/>,
-  <Card key='7'   cardID='7'  sendIdToDeck={receiveIdFromCard} image={'Joffrey_Baratheon.png'}/>,
-  <Card key='8'   cardID='8'  sendIdToDeck={receiveIdFromCard} image={'Karstark.png'}/>,
-  <Card key='9'   cardID='9'  sendIdToDeck={receiveIdFromCard} image={'Lannister.png'}/>,
-  <Card key='10'  cardID='10' sendIdToDeck={receiveIdFromCard} image={'Martell.png'}/>,
-  <Card key='11'  cardID='11' sendIdToDeck={receiveIdFromCard} image={'Mormont.png'}/>,
-  <Card key='12'  cardID='12' sendIdToDeck={receiveIdFromCard} image={'Night_Watch.png'}/>,
-  <Card key='13'  cardID='13' sendIdToDeck={receiveIdFromCard} image={'Payne.png'}/>,
-  <Card key='14'  cardID='14' sendIdToDeck={receiveIdFromCard} image={'Stannis_Baratheon.png'}/>,
-  <Card key='15'  cardID='15' sendIdToDeck={receiveIdFromCard} image={'Targaryen.png'}/>,
-  <Card key='16'  cardID='16' sendIdToDeck={receiveIdFromCard} image={'Tully.png'}/>,
-  <Card key='17'  cardID='17' sendIdToDeck={receiveIdFromCard} image={'Tyrell.png'}/> ,   ]
+  const arrayOfAllCards = [
+  <Card className='card' key='1'   cardID='1'  sendIdToDeck={receiveIdFromCard} image={'Arryn.png'}/>,
+  <Card className='card' key='2'   cardID='2'  sendIdToDeck={receiveIdFromCard} image={'Baratheon.png'}/>,
+  <Card className='card' key='3'   cardID='3'  sendIdToDeck={receiveIdFromCard} image={'Brynden.png'}/>,
+  <Card className='card' key='4'   cardID='4'  sendIdToDeck={receiveIdFromCard} image={'Clegane.png'}/>,
+  <Card className='card' key='5'   cardID='5'  sendIdToDeck={receiveIdFromCard} image={'direwolf.png'}/>,
+  <Card className='card' key='6'   cardID='6'  sendIdToDeck={receiveIdFromCard} image={'Greyjoy.png'}/>,
+  <Card className='card' key='7'   cardID='7'  sendIdToDeck={receiveIdFromCard} image={'Joffrey_Baratheon.png'}/>,
+  <Card className='card' key='8'   cardID='8'  sendIdToDeck={receiveIdFromCard} image={'Karstark.png'}/>,
+  <Card className='card' key='9'   cardID='9'  sendIdToDeck={receiveIdFromCard} image={'Lannister.png'}/>,
+  <Card className='card' key='10'  cardID='10' sendIdToDeck={receiveIdFromCard} image={'Martell.png'}/>,
+  <Card className='card' key='11'  cardID='11' sendIdToDeck={receiveIdFromCard} image={'Mormont.png'}/>,
+  <Card className='card' key='12'  cardID='12' sendIdToDeck={receiveIdFromCard} image={'Night_Watch.png'}/>,
+  <Card className='card' key='13'  cardID='13' sendIdToDeck={receiveIdFromCard} image={'Payne.png'}/>,
+  <Card className='card' key='14'  cardID='14' sendIdToDeck={receiveIdFromCard} image={'Stannis_Baratheon.png'}/>,
+  <Card className='card' key='15'  cardID='15' sendIdToDeck={receiveIdFromCard} image={'Targaryen.png'}/>,
+  <Card className='card' key='16'  cardID='16' sendIdToDeck={receiveIdFromCard} image={'Tully.png'}/>,
+  <Card className='card' key='17'  cardID='17' sendIdToDeck={receiveIdFromCard} image={'Tyrell.png'}/> ,   ]
+  
+  
+ // Fisher-Yates (Knuth) shuffle -- Unbiased shuffle
+  const shuffle = (array) => {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex !== 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+  let cardsToChooseFrom = []
+  const cardWithdrawer = (arr) => {
+    const array = []
+    for (let i = 0; i < props.cardsAllowed; i++) {
+      array.push(arr[i])
+    }
+    cardsToChooseFrom = array
+  }
+
+  cardWithdrawer(arrayOfAllCards)
+
+  useEffect(() => {
+
+  })
+
   return (<div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-6">
-      {arrayOfAllCards}
+      {shuffle(cardsToChooseFrom)}
     </div>
     )
 }
